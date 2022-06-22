bdbd543afc7349f534a98fda934e330e4b8e4e36
mindfling <dimendroider@gmail.com>
2022-06-22 18:59:55 +0300

    feat lesson01 module1 easy

---
+++ lesson01/lesson01_easy/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = 'Кроссовки';
+const productQuantity = 12;
+const productCategory = 'обувь';
+const productPrice = 1250;
+
+console.log('Наименование товара:', productName);
+
