fbdc0f994d7cbb9253e6a9fd3db1c81e26734755
mindfling <dimendroider@gmail.com>
2022-06-28 14:29:20 +0300

    feat lesson05 task03 for

--- lesson05/Task03.js
+++ lesson05/Task03.js
@@ -6,11 +6,35 @@
   Пример: "Привет мир" => "рим тевирП"
 */
 
+// * первый вариант
 // * самый простой декларативный способ
 const flipString = str => {
   str = str || '';
   return str.split('').reverse().join('');
 }
 
+// тест функции
 console.log(flipString("Привет мир"));
 console.log(flipString("Привет Перевернутая Срока"));
+console.log(flipString("TeNet палиндром ДоВод"));
+
+
+
+// * второй вариант
+// * перебор элементов строки в цикле for
+// здесь вариант аргумента по умолчанию
+function flipStringFor(str = '') {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i-- ) {
+    result += str[i];
+  }
+  return result;
+}
+
+// тест функции
+console.log(flipStringFor("Привет мир"));
+console.log(flipStringFor("Привет Перевернутая Срока"));
+console.log(flipStringFor("TeNet палиндром ДоВод"));
+
+
+
