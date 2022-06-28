ed2cf120500b05ae3b6233ec5baad28a06d66d3a
mindfling <dimendroider@gmail.com>
2022-06-28 09:41:28 +0300

    feat lesson05 task02

---
+++ lesson05/Task02.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+Вторая задача:
+  Напишите функцию, которая принимает строку и возвращает модифицированную строку
+  Строка начинается с большой буквы, остальные буквы маленькие
+  Пример:  "привет Мир" => "Привет мир"
+*/
+
+const capitalize = (str) => {
+  str = str || ''; // проверка на пустую строку
+  const copyStr = str.toLowerCase(); //переводим все символы в нижний регистр
+  const resultStr = copyStr.charAt(0).toUpperCase() + copyStr.substring(1); // первый символ строки верхнего регистра
+  return resultStr;
+}
+
+
+console.log(capitalize("привет Мир"));
+console.log(capitalize("прИвЕт мИр"));
+console.log(capitalize("СТРоКа ЗаГЛаВНоЙ БуКВы..."));
+console.log(capitalize());
