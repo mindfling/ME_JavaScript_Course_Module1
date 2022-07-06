9c58f63f0526d51a7f81070a055a0cf32b7dbf31
mindfling <dimendroider@gmail.com>
2022-07-06 15:22:55 +0300

    feat lesson08 task01 random variant 1..100

--- lesson08/task01.js
+++ lesson08/task01.js
@@ -7,26 +7,18 @@
   И возвращает массив со случайными числами от одного до 100 включительно;
 */
 
-// * пример на https://learn.javascript.ru/task/random-int-min-max
-function randomInteger(min, max) {
-  // получить случайное число от (min-0.5) до (max+0.5)
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
-}
 
-
-// * своя функция генерации случайных чисел от 0 до n
-// можно использовать одну из этих функций:
-const getRandiFloor = n => Math.floor( (n + 1) * Math.random() );
-const getRandiRound = n => Math.round( (n + 1) * Math.random() - 0.5 );
-const getRandiCeil  = n =>  Math.ceil( (n + 1) * Math.random() - 1 );
+// * своя функция генерации случайных чисел от 1 до n
+const getRandomInt = (min, max) => Math.floor( Math.random() * (max - min + 1) + min );
 
 
 
-const getRandomArray = (n) => {
+// * функция принимает количество элементов в массиве
+const getRandomArray = (nCount = 100) => {
   let arr = [];
-  for (let i = 0; i < n; i++) {
-    arr[i] = getRandiRound(100);
+  for (let i = 0; i < nCount; i++) {
+    // ? arr[i] = getRandomInt(100);
+    arr.push(getRandomInt(1, 100));
   }
   return arr;
 }
@@ -36,5 +28,6 @@ const getRandomArray = (n) => {
 console.log(getRandomArray(15));
 console.log(getRandomArray(100));
 console.log(getRandomArray(500));
+console.log(getRandomArray());
 
 
