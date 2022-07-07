de2af6e53a8037913990c83af2fb7b81c99343b1
mindfling <dimendroider@gmail.com>
2022-07-07 15:36:57 +0300

    feat lesson08 game02 hard

--- lesson08/game_hard/game02.js
+++ lesson08/game_hard/game02.js
@@ -5,36 +5,61 @@ const getRandomInt = (min, max) => Math.floor( Math.random() * (max - min + 1) +
 
 const isNumber = (n) => !isNaN(n) && isFinite(n);
 
-// todo еще проверка на число здесь
-const n = +prompt('Введите первое число диапазона min');
-const m = +prompt('Введите второе число диапазона max');
 
-const minNumb = (n < m) ? n : m;
-const maxNumb = (n > m) ? n : m;
+const promptNumber = (str) => {
+  let n;
+  do {
+    n = prompt(str);
+    if (!isNumber(n)) {
+      console.log('n: ', n);
+      alert('Вы ввели не число');
+      continue;
+    }
+    if (n === null) {
+      return false;
+      break;
+    }
+  } while( !isNumber(n) );
 
-const userNumbers = []; // todo use numberSet = new Set();
+  return parseInt(n);
+}
 
 
 // * начало игрового цикла
 do {
+  // todo еще проверка на число здесь
+  const n = promptNumber('Введите первое число диапазона min');
+  const m = promptNumber('Введите второе число диапазона max');
+
+  const minNumb = (n < m) ? n : m;
+  const maxNumb = (n > m) ? n : m;
+
+  const delta = Math.abs(maxNumb - minNumb); // количество чисел в диапазоне
+  let triesCount = Math.round(delta * 0.30); // количество попыток 30%
+
+  // todo use numberSet = new Set();
+  const userNumbersList = []; //список чисел которые ввел кользователь
+
   const botNumber = getRandomInt(minNumb, maxNumb);
   console.log('Бот загадал число: ', botNumber);
 
+  //число которое вводит пользователь
   let userNumber;
-  while ( (userNumber = prompt('Угадайте целое число от 1 до 100')) && (userNumber != botNumber) ) {
+  // если пользователь нажимает Cancel -> prompt возвращает null
+  while ( (userNumber = prompt(`Угадайте целое число от ${minNumb} до ${maxNumb}`)) && (userNumber !== null) ) {
 
     if (isNumber(userNumber)) {
       //ввели число      
       //todo проверка масс на это число
-      if (userNumbers.includes(userNumber)) {
-        // число уже в массиве
-        console.log('Вы уже вводили это число,', userNumber, 'Попробуйте еще раз');
-        alert('Вы уже вводили это число,', userNumber, 'Попробуйте еще раз');
+      if (userNumbersList.includes(userNumber)) {
+        // число уже в массиве, попытка не защитывается
+        console.log('Это число вы уже вводили. Попробуйте еще раз');
+        alert('Это число вы уже вводили,', userNumber, 'Попробуйте еще раз');
         continue;
       } else {
         console.log('Вы ввели новое число,',  userNumber);
-        userNumbers.push(userNumber);
-        console.log(userNumbers);
+        userNumbersList.push(userNumber);
+        console.log(userNumbersList);
       }
 
     } else {
@@ -45,21 +70,22 @@ do {
     }
 
     if ( botNumber > userNumber ) {
-      console.log('Загаданное число должно быть Больше');
+      console.log('Загаданное число Больше вашего');
       alert('Больше');
     } else if ( botNumber < userNumber ) {
-      console.log('Загаданное число должно быть Меньше');
+      console.log('Загаданное число Меньше вашего');
       alert('Меньше');
-    } else {
+    } else if (+userNumber == botNumber ) {
       console.log('Правильно! Угадал...');
       alert('Правильно! Угадал...');
+      break;
     }
     
   }
   
-  console.log('Правильно! Угадал! Конец игры');
-  alert('Правильно! Угадал! Конец игры');
+  console.log('Конец игры');
+  alert('Конец игры');
 
 } while (confirm('Продолжить игру?'));
 
-console.log('Конец игры, чтобы начать заново, перезагрузите страницу');
+console.log('Игра окончена, чтобы начать заново, перезагрузите страницу');
