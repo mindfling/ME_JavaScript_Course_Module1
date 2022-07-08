68f3fdf0f996fda19237e6c4bea0b5f4a40c65a7
mindfling <dimendroider@gmail.com>
2022-07-09 01:24:44 +0300

    feat lesson10 getUniqueCountSimple by Set()

--- lesson10/task_ipv4/script.js
+++ lesson10/task_ipv4/script.js
@@ -5,30 +5,52 @@
 задача: написать функцию, которая будет возвращать число равное количеству уникальных ip-адресов
 */
 
+// * массив listIPv4 загружем в предыдущем скрипте
 
+
+// * самый простой способ с помощью Set() коллекции
+const getUniqueCountSimple = (list = []) => {
+  if (Array.isArray(list)) {
+    let setIP = new Set(list); // создаем коллекцию из массива
+    return setIP.size; // возвращает количество уникальных элементов в коллекции
+  }
+  return; // если на входе был не массив
+}
+
+console.log(listIPv4);
+console.log(listIPv4.length, '<- всего элементов в массиве');
+console.log(getUniqueCountSimple(listIPv4), '<- количество уникальных элементов в коллекции');
+
+
+
+/*
 const len = listIPv4.length;
-
-const ipSet = new Set();
+const setIP = new Set(); // set для подсчета уникальных адресов
 let uniqueCount = 0;
 let repeatCount = 0;
-
+// подсчет в цикле
 listIPv4.forEach( oneIP => {
-  if (ipSet.has(oneIP)) {
-    repeatCount++;
+  if (setIP.has(oneIP)) {
+    repeatCount++; // подсчет повторяющихся адресов
   } else {
-    uniqueCount++;
-    ipSet.add(oneIP);
+    uniqueCount++; // подсчет списка неповторяющихся адресов
+    setIP.add(oneIP);
   }
 });
+*/
 
-
-console.log('исходный массив listIPv4: ', listIPv4);
-console.log('исходное количество элементов: ', len);
-
-console.log('множестово SetIPv4: ', ipSet);
-console.log('количество элементов множества size: ', ipSet.size);
-console.log('уникальные uniqueCount: ', uniqueCount);
-console.log('повторяющиеся repeatCount: ', repeatCount);
-
-
-
+/*
+// console.log('исходный массив listIPv4: ', listIPv4);
+console.log('исходное количество адресов в списке:', len);
+// console.log('множестово SetIPv4: ', setIP);
+console.log('количество элементов множества size:', setIP.size, '= количеству неповторяющихся адресов');
+console.log('уникальные uniqueCount:', uniqueCount);
+console.log('повторяющиеся repeatCount:', repeatCount);
+*/
+// todo Записать видео с моим вопросом
+//? 
+//? уникальные адреса ?
+//? неповторяющиеся адреса ?
+//? неуникальные адреса ?
+//? повторяющиеся адреса ?
+//? 
