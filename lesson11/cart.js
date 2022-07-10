f88e46b5405b3ed17800b38df44867cc3f858920
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-07-10 16:09:17 +0300

    feat eslint cart.js

--- lesson11/cart.js
+++ lesson11/cart.js
@@ -1,86 +1,66 @@
 'use strict';
 
 
-
-// * формируем объект корзины покупок cart
 const cart = {
 
-  // * свойства
-  items: [],         // пустой массив список товаров
-  count: 0,         // количество товаров
-  _totalPrice: 0,  // общая стоимость корзины
+  // ** свойства **
+  items: [], // пустой массив список товаров
+  count: 0, // количество товаров
+  _totalPrice: 0, // общая стоимость корзины
 
 
   // *** методы ***
-  //    - получить общую стоимость товаров - метод возвращает значение свойства //? _totalPrice
+
+  // * getter totalPrice
   get totalPrice() {
-    console.log('getter for totalPrice');
     return this.calculateItemPrice();
   },
 
-  // ? ВОПРОС почему этот метод не геттер ?
-  // getTotalPrice() {
-  //   return this.totalPrice;
-  // },
-
 
   //  добавить товар
-  /*     Принимает три параметра:
-  *  название товара //?  productName 
-  *  цену товара      //?   productPrice
-  *  количество товара (опциональный параметр, по умолчанию 1 товар) //?   productAmount
-   этот метод формирует объект из полученных параметров и добавляет его в свойство items
-   также вызывает все необходимые методы чтобы свойства count и _totalPrice были актуальные
-  */
-  add( productName, productPrice, productAmount = 1 ) {
+  add(productName, productPrice, productAmount = 1) {
     const product = {
-      name: productName, // * название товара
-      price: productPrice, // * цена товара
-      amount: productAmount // * количество товаров
-    }
-    this.items.push(product);          // добавляем товар в список
-    this.increaseCount(productAmount); // увеличиваем счетчик количества товаров в корзине
-    this.calculateItemPrice();         // вычисляем общую стоимость корзины
+      name: productName, // название товара
+      price: productPrice, // цена товара
+      amount: productAmount, // количество товаров
+    };
+    this.items.push(product); // добавляем товар в список
+    this.increaseCount(productAmount); // увеличиваем счетчик количества товаров
+    this.calculateItemPrice(); // вычисляем общую стоимость корзины
   },
 
 
   // увеличить количество товаров
-  // Увеличивает свойство count на num
   increaseCount(num = 1) {
     this.count += num;
   },
 
 
-  // пересчитывает стоимость всей корзины и записывает значение в //? _totalPrice 
+  // вычисляем сумму всей корзины
   calculateItemPrice() {
-    // вычисляем сумму
-    let total = this.items.reduce( (sum, curItem) => {
+    const total = this.items.reduce((sum, curItem) => {
+      console.log('sum: ', sum);
       return sum + curItem.price * curItem.amount;
     }, 0);
-    this._totalPrice = total; // сохраняем
-
+    this._totalPrice = total; // сохраняем в _totalPrice
     return total;
   },
 
 
-  // очищает полностью нашу корзину, возвращает все значения в изначальные
+  // очищает полностью нашу корзину
   clear() {
-    console.log('очищает полностью нашу корзину');
     this.items = [];
     this.totalPrice = 0;
     this.count = 0;
   },
 
 
-  // распечатать вывести в консоль содержание корзины //* print
+  // выводит содержимое корзины в консоль
   print() {
     console.log(JSON.stringify(this));
   },
+};
 
-}
-
-
-// *****************************************
 
 console.log('пустая корзина cart: ', cart);
 cart.print();
@@ -89,8 +69,7 @@ cart.add('Носки', 150, 5);
 cart.add('Куртка', 1150, 7);
 cart.add('Кроссовки', 1250, 1);
 cart.add('Скейтборд', 2550, 2);
-//в итоге получаем корзину и несколько товаров
 
+console.log('корзина с товарами cart: ', cart);
 cart.print();
-// console.log('корзина cart: ', cart);
 
