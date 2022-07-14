132992c5fd6be04cbee89463d23bb9d249e06089
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-07-14 20:07:17 +0300

    fix eslint lesson07

--- lesson07/Task03.js
+++ lesson07/Task03.js
@@ -2,37 +2,72 @@
 
 /*
 * Третья задача * :
-  Напишите функцию 
-  ? addPrefix 
+  Напишите функцию
+  ? addPrefix
   Функция получает массив в виде параметров и возвращает массив с префиксами
   После префикса пробел добавляется автоматически
 
 * Входящий массив :
-?  const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+const names = [
+  'Noah',
+  'Liam',
+  'Mason',
+  'Jacob',
+  'Robot',
+  'William',
+  'Ethan',
+  'Michael',
+  'Alexander',
+];
 * Вызов функции:
 ?  addPrefix(names, 'Mr')
 * Результат функции:
-?  ['Mr Noah', 'Mr Liam', 'Mr Mason', 'Mr Jacob', 'Mr. Robot', 'Mr William', 'Mr Ethan', 'Mr Michael', 'Mr Alexander'];
+? a = [
+  'Mr Noah',
+  'Mr Liam',
+  'Mr Mason',
+  'Mr Jacob',
+  'Mr. Robot',
+  'Mr William',
+  'Mr Ethan',
+  'Mr Michael',
+  'Mr Alexander',
+];
 */
-
 
 // функция принимает на входе массив с именами и префикс
 // возвращает массив имен с префиксами
 const addPrefix = (namesList, prefix = 'Mr.') => {
-
   if (!Array.isArray(namesList)) {
     return;
   }
 
-  return namesList.map( item => `${prefix} ${item}` );
-} 
-
+  return namesList.map(item => `${prefix} ${item}`);
+};
 
 // Тест
-const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
-const namesRus = ['Иванчей', 'Пьеров', 'Сидоров', 'Каземцев', 'Смирнов', 'Соколов', 'Паприков'];
+const names = [
+  'Noah',
+  'Liam',
+  'Mason',
+  'Jacob',
+  'Robot',
+  'William',
+  'Ethan',
+  'Michael',
+  'Alexander',
+];
+const namesRus = [
+  'Иванчей',
+  'Пьеров',
+  'Сидоров',
+  'Каземцев',
+  'Смирнов',
+  'Соколов',
+  'Паприков',
+];
 
 console.log();
-console.log( 'output :', addPrefix(names), '\n\n' );
-console.log( 'output :', addPrefix(names, 'Mr'), '\n\n' );
-console.log( 'output :', addPrefix(namesRus, 'Господин'), '\n\n' );
+console.log('output :', addPrefix(names), '\n\n');
+console.log('output :', addPrefix(names, 'Mr'), '\n\n');
+console.log('output :', addPrefix(namesRus, 'Господин'), '\n\n');
--- lesson07/Task_hard.js
+++ lesson07/Task_hard.js
@@ -2,57 +2,64 @@
 
 /*
 Не обязательное задание (+1 балл)
-
-  Напишите функцию 
+  Напишите функцию
   * getAveragePriceGoods
   для получения средней цены товара
-
   * Дан многомерный (двумерный) массив :
 */
 const allCashbox = [
-  [12, 4500], [7, 3210], [4, 650], [3, 1250], [9, 7830], [1, 990], [6, 13900], [1, 370]
+  [12, 4500],
+  [7, 3210],
+  [4, 650],
+  [3, 1250],
+  [9, 7830],
+  [1, 990],
+  [6, 13900],
+  [1, 370],
 ];
 
 /*
-? В каждом массиве из двух элементов, 
-  первым - является количество товаров в чеке, 
+? В каждом массиве из двух элементов,
+  первым - является количество товаров в чеке,
   а вторым — общая сумма.
 ? Необходимо посчитать среднюю стоимость одного товара в магазине.
 */
 
-
-
-const getAveragePriceGoods = (goodsList) => {
-
-  // проворяем аргумент должен быть массив
+const getAveragePriceGoods = goodsList => {
   if (!Array.isArray(goodsList)) {
     return;
   }
-
-  let len = goodsList.length; // длина массива == количество елементов
-  
-  let oneGoodPriceList = goodsList.map( (oneGood) => {
-    // формируем массив стоимостей каждого товара
-    // делим общую сумму на количество товара
-    return Math.floor(oneGood[1] / oneGood[0] * 100) / 100; 
-  });
-
+  const len = goodsList.length; // длина массива == количество елементов
+  // формируем массив стоимостей каждого товара
+  // делим общую сумму на количество товара
+  const oneGoodPriceList = goodsList.map(oneGood =>
+    Math.floor(oneGood[1] / oneGood[0] * 100) / 100,
+  );
   // подсчитываем сумму элементов массива
   let sumPrices = 0;
-  oneGoodPriceList.forEach( onePrice => {
+  oneGoodPriceList.forEach(onePrice => {
     sumPrices += onePrice;
   });
-
   // вычисляем среднюю цену товаров в магазине
-  let averagePrice = Math.floor( sumPrices / len * 100 ) / 100;
+  const averagePrice = Math.floor(sumPrices / len * 100) / 100;
 
   return averagePrice;
-}
-
+};
 
 
 // тест
 console.log();
-console.log( getAveragePriceGoods(allCashbox) );
+console.log(getAveragePriceGoods(allCashbox));
 console.log();
-console.log( getAveragePriceGoods([[17, 4500], [7, 3210], [4, 650], [3, 1250], [18, 9830], [1, 990], [16, 13900], [1, 370]]) );
+console.log(
+  getAveragePriceGoods([
+    [17, 4500],
+    [7, 3210],
+    [4, 650],
+    [3, 1250],
+    [18, 9830],
+    [1, 990],
+    [16, 13900],
+    [1, 370],
+  ]),
+);
