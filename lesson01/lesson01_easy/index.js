ba9ffee273ca4915e9fe24d362a8e1eb2b777abe
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-07-14 21:06:37 +0300

    clean lesson09 123

--- lesson01/lesson01_easy/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const productName = 'Кроссовки';
-const productQuantity = 12;
-const productCategory = 'обувь';
-const productPrice = 1250;
-
-console.log('Наименование товара:', productName);
-
